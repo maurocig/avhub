ba25bb525262ddb19a3e64bf03e56a6fb08f91ee
Mauro Cigliuti <mcigliuti01@gmail.com>
2023-03-20 19:34:32 -0300

    order schema

---
+++ models/schemas/Order.schema.js
@@ -0,0 +1,15 @@
+const { Schema } = require('mongoose');
+
+const OrderSchema = new Schema({
+  orderNumber: {
+    type: Number,
+  },
+  generated: {
+    type: Boolean,
+    default: true,
+  },
+  cartId: {
+    type: Schema.Types.ObjectId,
+    ref: 'carts',
+  },
+});
